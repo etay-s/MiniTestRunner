c0acdf7e372a5286e0a567678b466c8715ac5054
Etay Schwartzberg <etay.schwartzberg@post.idc.ac.il>
2023-01-24 04:01:41 +0200

    Fixed runner scripts to run by relative path

--- runner.cmd
+++ runner.cmd
@@ -1,4 +1,4 @@
 
-"C:\Users\user\Desktop\Tester\TestRunner\bin\Debug\net6.0\TestRunner.exe" "C:\Users\user\Desktop\Tester\DeveloperCodeAndTests\bin\Debug\net6.0\DeveloperCodeAndTests.dll"
+".\TestRunner\bin\Debug\net6.0\TestRunner.exe" ".\DeveloperCodeAndTests\bin\Debug\net6.0\DeveloperCodeAndTests.dll"
 
 PAUSE
--- runner.sh
+++ runner.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-exe="C:/Users/user/Desktop/Tester/TestRunner/bin/Debug/net6.0/TestRunner.exe"
+exe="./TestRunner/bin/Debug/net6.0/TestRunner.exe"
 
-dll="C:/Users/user/Desktop/Tester/DeveloperCodeAndTests/bin/Debug/net6.0/DeveloperCodeAndTests.dll"
+dll="./DeveloperCodeAndTests/bin/Debug/net6.0/DeveloperCodeAndTests.dll"
 
 $exe $dll
 
